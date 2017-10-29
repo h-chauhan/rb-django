b25b9577779be8ea3bc775fede0d87699ff3279e
Himanshu Chauhan <hchauhan1404@outlook.com>
2017-10-29 19:09:13 +0530

    new db

--- rajbhog/settings.py
+++ rajbhog/settings.py
@@ -78,13 +78,24 @@ WSGI_APPLICATION = 'rajbhog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('ENV_TYPE') == 'Heroku':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd1lmni035u1m28',
+            'USER': 'dnnjazxufyvqud',
+            'PASSWORD': '3d313b977e82895e1ca176865ddc85fbc6d398b3a6ccbcaec6cb9c226ca7fc7c',
+            'HOST': 'ec2-23-23-249-169.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
